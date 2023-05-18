1983c0d23df194d2b6f8a0e6b372574bb2295352
Ziyuan Wang <zwang590@ucsc.edu>
2023-05-17 23:47:37 -0700

    progress until 58:46

--- src/Overworld.js
+++ src/Overworld.js
@@ -1,6 +1,8 @@
 class Overworld extends Phaser.Scene{
     constructor (){
         super({key: 'overworldScene'})
+
+        this.VEL = 100
     }
 
     preload(){
@@ -21,9 +23,46 @@ class Overworld extends Phaser.Scene{
         const bglayer = map.createLayer('background',tileset,0,0)
         const terrainLayer = map.createLayer('terrain',tileset,0,0)
         const trees = map.createLayer('trees',tileset,0,0)
+    
+        //add player
+        this.slime = this.physics.add.sprite(32,32,'slime',0)
+        this.anims.create({
+            key: 'jiggle',
+            frameRate: 8,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('slime',{
+                start: 0,
+                end: 1
+            }) 
+        })
+        this.slime.play('jiggle')
+
+        //collide with world
+        this.slime.setCollideWorldBounds(true)
+
+        //cameras
+        this.cameras.main.setBounds(0,0,map.widthInPixels,map.heightInPixels)
+        this.cameras.main.startFollow(this.slime,true,0.25,0.25)
+        this.physics.world.bounds.setTo(0,0,map.widthInPixels,map.heightInPixels)
+        //INPUT
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     update(){
+        this.direction = new Phaser.Math.Vector2(0)
+        if (this.cursors.left.isDown){
+            this.direction.x = -1
+        }else if(this.cursors.right.isDown){
+            this.direction.x = 1
+        }
+
+        if (this.cursors.up.isDown){
+            this.direction.y = -1
+        }else if(this.cursors.down.isDown){
+            this.direction.y = 1
+        }
+        this.direction.normalize()
+        this.slime.setVelocity(this.VEL * this.direction.x,this.VEL*this.direction.y)
 
     }
 }
