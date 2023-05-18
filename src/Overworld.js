04ae7e4138f3f11f5bfe0419a044d2543bdc1d02
Ziyuan Wang <zwang590@ucsc.edu>
2023-05-17 23:29:29 -0700

    progress until 44:14

--- src/Overworld.js
+++ src/Overworld.js
@@ -3,19 +3,24 @@ class Overworld extends Phaser.Scene{
         super({key: 'overworldScene'})
     }
 
-    perload(){
+    preload(){
         this.load.path='./assets/'
         this.load.spritesheet('slime','slime.png',{
             frameWidth: 16,
             frameHeight: 16
         })
         this.load.image('tilesetImage','tileset.png')
-        this.load.tilemapTiledJSON('tilemap.json','walk-through.json')
+        this.load.tilemapTiledJSON('tilemapJSON','area01.json')
     }
 
     create(){
-        const map = this.add.tilemap('tilemap.json')
-        
+        const map = this.add.tilemap('tilemapJSON')
+        const tileset = map.addTilesetImage('tileset','tilesetImage')
+
+        //add layer
+        const bglayer = map.createLayer('background',tileset,0,0)
+        const terrainLayer = map.createLayer('terrain',tileset,0,0)
+        const trees = map.createLayer('trees',tileset,0,0)
     }
 
     update(){
